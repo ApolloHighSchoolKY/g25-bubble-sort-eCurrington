516490ca3ac35cc8fae1b59889c71ed2544b02e5
eCurrington <eli.currington@stu.daviess.kyschools.us>
2025-04-17 12:49:32 +0000

    done

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,8 +6,9 @@ public class BubbleSort
     int[] myNums = {12, 15, 0, 44, 13, 1, 2};
 		int passes;
 		int comparisons;
-		int tempNum;
+		int tempNum; 
 
+		
 		//Loop once for each pass, where passes is one less than the number of items.
 			//Loop once for each comparison, where comparisons are one less than the number of unsorted.
 				//If they are out of order, swap the values
